92dd7fd971785af08177d28c9223cb9dbd518004
Vishal Prajapati <vishal.prajapati@trootech.com>
2024-05-22 14:08:23 +0530

    remove unused helper file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,8 @@
-import {Helper} from './Helper';
 import NavigationService from './NavigationService';
 import * as Utility from './Utility';
 
 const Utils = {
   Utility,
   NavigationService,
-  Helper,
 };
 export {Utils};
